28b8ee922569fa08015927b9f185eca1c4c6c3f4
Frank Sepulveda <socieboy@gmail.com>
2018-02-16 12:52:57 -0800

    wip

--- app/Services/Network/ReadInterfaces.php
+++ app/Services/Network/ReadInterfaces.php
@@ -73,6 +73,7 @@ trait ReadInterfaces
     {
 //        $output = is_local_envorioment() ? $this->commands[$value] . ':                            192.11.88.1' . PHP_EOL : shell_exec('nmcli device show ' . $this->device . ' | grep ' . $this->commands[$value]);
         $output = shell_exec('nmcli device show ' . $this->device);
+        Log::inf($output);
         $output = array_map('trim', explode(':', $output)[1]);
         Log::info($output);
         return $output;
