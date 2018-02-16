2cf7613823966419ceaa969e789e02d0c846edc6
Frank Sepulveda <socieboy@gmail.com>
2018-02-16 14:45:14 -0800

    wip

--- app/Services/Network/NetworkInterface.php
+++ app/Services/Network/NetworkInterface.php
@@ -167,11 +167,14 @@ class NetworkInterface
      */
     public function ping($endpoint = 'google.com')
     {
-        $command = 'ping -I ' . $this->device . ' -w 10 -c 1 ' . $endpoint;
-        exec($command, $result);
-        Log::info($result);
+//        $command = 'ping -I ' . $this->device . ' -w 10 -c 1 ' . $endpoint;
+//        exec($command, $result);
+//        Log::info($result);
+//        $result = implode(' ', $result);
+//        return (str_contains($result, ' bytes from ' . $this->ip_address . ' ' . $this->device . ':') && str_contains($result, '0% packet loss')) ? true : false;
+        exec('ping -I ' . $this->device . ' -w 10 -c 1 ' . $endpoint, $result);
         $result = implode(' ', $result);
-        return (str_contains($result, ' bytes from ' . $this->ip_address . ' ' . $this->device . ':') && str_contains($result, '0% packet loss')) ? true : false;
+        return (str_contains($result, ' bytes from ') && str_contains($result, '0% packet loss')) ? true : false;
     }
 
     /**
